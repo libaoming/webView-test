4f73ab393e9779e580e10d49cec3696ef0d538b3
master <libaoming2@gmail.com>
2016-08-18 13:03:46 +0800

    complete webview test

--- WKwebview-test/ViewController.swift
+++ WKwebview-test/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var container: UIView!
+    
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView = WKWebView()
+        container.addSubview(webView)
+        
+        print(container.bounds.width)
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        
+        webView.frame = frame
+        
+        
+       loadRequest("http://www.apple.com/")
+
     }
 
-
+    @IBAction func loadSwift(sender: AnyObject) {
+        loadRequest("https://developer.apple.com/swift/blog/")
+    }
+    
+    @IBAction func loadGossip(sender: AnyObject) {
+        loadRequest("http://www.celebrity-gossip.net/")
+    }
+   
+    
+    @IBAction func loadSteak(sender: AnyObject) {
+        loadRequest("http://www.thekitchn.com/how-to-cook-perfect-steak-in-the-oven-108490")
+    }
+    
+    func loadRequest(reqStr: String){
+        
+        let request = NSURLRequest(URL: NSURL(string: reqStr)!)
+        
+        webView.loadRequest(request)
+        
+        
+    }
 }
 
